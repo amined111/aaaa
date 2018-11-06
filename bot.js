2156e74f7574036551d4f57b4e0ced5d12f3d492
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-06 16:58:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,16 @@ client.user.setGame(`#bc | A.`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
-
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`A.`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
 
 client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN1);
