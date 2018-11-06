c69e61bfcafaafb71d1023ef0cfc89bf54938e94
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-06 16:20:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,5 +28,44 @@ client.user.setStatus("dnd")
 });
 
 
+const Eris = require("eris");
+var kboosh = new Eris("NTA5MzUzMzA0Nzc4NzM1NjE3.DsNBPQ.YWJDGEhPt5qTqLS8KU26r6vH5JA");
+var kboosh_id = "498250817695121418";
+                    var i = "0";
+                    var x = "0";
+kboosh.on("voiceChannelJoin", (msg) => {
+    x++;
+    kboosh.editChannel(kboosh_id, { name : "Voice ⇏「" + x + "」"});
+});
+kboosh.on("voiceChannelLeave", (msg) => {
+    x--;
+    kboosh.editChannel(kboosh_id, { name : "Voice ⇏「" + x + "」"});
+});
+
+kboosh.on("messageCreate", (msg) => {
+    if(msg.author.id !== "485917652120764429") return kboosh.createMessage('__**This Command is only for the bot Owner**__');
+    if(msg.content === "Avoice") {
+        let users = msg.channel.guild.members.map(m => m.user.id);
+        let messages = [];
+        messages.push(users);
+        setTimeout(function(){
+        while (i <= messages[0].length - 1) {
+            check = msg.channel.guild.members.get(messages[0][i]);
+        if(!check.voiceState.channelID){
+                i++;
+        }else{
+                x++;
+                i++;
+        }
+}
+    console.log(x);
+    kboosh.createMessage(msg.channel.id, "Voice Online Members Now Are: **"+x+"** Members!");
+    kboosh.editChannel(kboosh_id, { name : "Voice ⇏「"+x+"」"});
+    messages = [];
+}, 1);
+    }
+});
+
+
  
 client.login(process.env.BOT_TOKEN);
