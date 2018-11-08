13609614525095cb9b4e2e40951d3ad5d594e84b
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-08 19:05:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,27 @@ client.user.setGame(`#bc | A.`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "help")) {
+let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **#help-1** ' ,' **اوامر اداريه** ')
+.addField('     **#help-2**  ' ,' **اوامر عامه** ')
+.setColor('RANDOM')
+message.channel.sendEmbed(embed);
+}
+});
+
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "help-1")) {
+let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **#clear** ' ,**مسح الشات** ')
+.addField('     **#bc**  ' ,' **برودكاست** ')
+.setColor('RANDOM')
+message.channel.sendEmbed(embed);
+}
+});
+
 
 client.login(process.env.BOT_TOKEN1);
