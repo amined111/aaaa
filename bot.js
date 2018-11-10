57a5397a471232c7a31e3d55d89c20debe5cd00c
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-10 23:54:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,4 +90,33 @@ client.on('guildMemberAdd', member => {
   });
 });
 
+
+client.on("message", message => {
+        var prefix = "-";// البرفكس
+    if(message.content.startsWith(prefix + "setwlc")) {
+        let args = message.mentions.channels.first();
+            if(!args) message.channel.send("** منشن روم . :x:**").then(m => {    
+m.delete(1500);
+})
+                if(!message.guild.member(message.author.id).hasPermission("MANAGE_CHANNELS")) return message.channel.send("**ليس لديك صلاحيات . :x:**");
+                        message.channel.send(`**${args}. لقد تم شغل الروم هذا للترحيب.**`);
+                    client.on("guildMemberAdd", (member) => {
+                            if(member.user.bot) return;
+                         var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle('New Member')
+    .setDescription('Welcome To Server')
+    .addField('**ID Member:',"" +  member.user.id, true)
+    .addField('**Tage Member:', member.user.discriminator, true)
+    .addField('Created At Member', member.user.createdAt, true)
+    .addField(' :bust_in_silhouette:  Your Number',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('GREEN')
+    .setFooter(member.guild.name, member.guild.iconURL, true)
+                         
+   args.send({embed : embed});
+                    });
+    }
+});
+
 client.login(process.env.BOT_TOKEN1);
